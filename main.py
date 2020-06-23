19bc1297ea57ceb034ff5c17cdea6737dca8ad83
Royer G <h4ck3r@DESKTOP-J9KKJEK.localdomain>
2020-06-23 12:49:42 -0500

    fixing: while true cycle

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from server import Server
 
 def main():
     servidor = Server()
-    while True:    
-        if servidor.get_cpu_percent() >= 95.0:
-            processes = servidor.get_processes()
-            processes_found = [servidor.get_process(ps['pid']) for ps in processes if ps['name'] == 'python3']
-            for ps in processes_found:
-                servidor.kill_process(ps)
+    if servidor.get_cpu_percent() >= 95.0:
+        processes = servidor.get_processes()
+        processes_found = [servidor.get_process(ps['pid']) for ps in processes if ps['name'] == 'python3']
+        for ps in processes_found:
+            servidor.kill_process(ps)
 
 
 if __name__ == '__main__':
